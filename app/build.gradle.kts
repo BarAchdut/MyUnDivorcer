f1a4bfacf89e17982d14fa8b2893d4b47ef05b19
Bar Achdut <bar.achdut@justeattakeaway.com>
2025-01-17 12:08:05 +0200

    Fix build errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.myundivorcer"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.myundivorcer"
