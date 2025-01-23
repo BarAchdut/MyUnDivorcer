c071d200e9a76b443cb79f97f0150edfbb0db4ac
Bar Achdut <bar.achdut@justeattakeaway.com>
2025-01-23 11:25:29 +0200

    Rom div

--- app/src/main/java/com/example/myundivorcer/MainActivity.kt
+++ app/src/main/java/com/example/myundivorcer/MainActivity.kt
@@ -16,7 +16,7 @@ import android.app.Application
 import com.google.firebase.database.FirebaseDatabase
 
 
-class MainActivity : Application() {
+class MyUnDivorcer : Application() {
     override fun onCreate() {
         super.onCreate()
         FirebaseDatabase.getInstance().setPersistenceEnabled(true)
